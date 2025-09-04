8eb1b03924f9c8ce8db8095a6209c9a78f9bbb3b
ShamSHa0987 <your_github_email@example.com>
2025-09-04 13:30:50 +0530

    test.tf

--- test.tf
+++ test.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
   ami           = "ami-051f7e7f6c2f40dc1"  # ✅ valid for us-east-1
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"i
 
   tags = {
     Name = "MyEC2Instance"
